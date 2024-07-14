1f10705caa743d6bf9fa5be1dab6f3827cde4686
Artem Kuznetsov <artemkuz.wmg@gmail.com>
2024-07-14 16:45:57 +0300

    update page layout

--- src/NewTabPage.tsx
+++ src/NewTabPage.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react'
+
 import { BookmarksGrid } from './components/bookmarks-grid'
+import NewBookmark from './components/modals/NewBookmark.modal'
 
 function TabPage () {
+  const [modalOpen, setModalOpen] = useState<boolean>(false)
+
+  function toggleModal () {
+    setModalOpen(prev => !prev)
+  }
+
   return (
-    <BookmarksGrid />
+    <>
+      <div className='veil' data-open={modalOpen}></div>
+      <NewBookmark isOpen={modalOpen} handleToggleModal={toggleModal} />
+      <BookmarksGrid handleToggleModal={toggleModal} />
+    </>
   )
 }
 
